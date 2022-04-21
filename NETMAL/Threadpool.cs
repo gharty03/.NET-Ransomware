220a356234014dfc648ff1926ae2734f3d7a7266
Garrett Harty <87092943+gharty03@users.noreply.github.com>
2022-04-21 00:58:47 -0700

    starting to implement multithreading

---
+++ NETMAL/Threadpool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NETMAL
+{
+    class Threadpool
+    {
+        private int maxThreads;
+        private int minThreads;
+        private int avaThreads;
+
+
+    }
+}
